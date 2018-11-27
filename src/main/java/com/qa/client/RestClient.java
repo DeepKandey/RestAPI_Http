ac236739bf6010a95037fb4818d7f21e15190bf8
DeepKandey <deepakggsipu@gmail.com>
2018-11-27 12:04:23 +0530

    minor changes

--- src/main/java/com/qa/client/RestClient.java
+++ src/main/java/com/qa/client/RestClient.java
@@ -13,41 +13,43 @@ import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 
 public class RestClient {
-	
-	//GET method without header
+
+	// GET method without header
 	public CloseableHttpResponse get(String url) throws ClientProtocolException, IOException {
-		CloseableHttpClient httpclient=HttpClients.createDefault(); //create http connection
-		HttpGet httpGet= new HttpGet(url); //http get request
-		CloseableHttpResponse closeableHttpResponse=httpclient.execute(httpGet); //hit the Get URL
+		CloseableHttpClient httpclient = HttpClients.createDefault(); // create http connection
+		HttpGet httpGet = new HttpGet(url); // http get request
+		CloseableHttpResponse closeableHttpResponse = httpclient.execute(httpGet); // hit the Get URL
 		return closeableHttpResponse;
 	}
-	
-	//GET method with headers
-	public CloseableHttpResponse get(String url,HashMap<String,String> headerMap) throws ClientProtocolException, IOException {
-		CloseableHttpClient httpclient=HttpClients.createDefault(); //create http connection
-		HttpGet httpGet= new HttpGet(url); //http get request
-		
-		//Adding header to get request
-		for(Map.Entry<String,String> entry:headerMap.entrySet()) {
+
+	// GET method with headers
+	public CloseableHttpResponse get(String url, HashMap<String, String> headerMap)
+			throws ClientProtocolException, IOException {
+		CloseableHttpClient httpclient = HttpClients.createDefault(); // create http connection
+		HttpGet httpGet = new HttpGet(url); // http get request
+
+		// Adding header to get request
+		for (Map.Entry<String, String> entry : headerMap.entrySet()) {
 			httpGet.addHeader(entry.getKey(), entry.getValue());
 		}
-		
-		CloseableHttpResponse closeableHttpResponse=httpclient.execute(httpGet); //hit the Get URL
+
+		CloseableHttpResponse closeableHttpResponse = httpclient.execute(httpGet); // hit the Get URL
 		return closeableHttpResponse;
 	}
-	
-	//POST method with headers
-	public CloseableHttpResponse post(String url,String entityString,HashMap<String,String> headerMap) throws ClientProtocolException, IOException {
-		CloseableHttpClient httpclient=HttpClients.createDefault(); //create http connection
-		HttpPost httpPost= new HttpPost(url); //http post request
-		httpPost.setEntity(new StringEntity(entityString)); //for payload
-			
-		//Adding header to get request
-		for(Map.Entry<String,String> entry:headerMap.entrySet()) {
+
+	// POST method with headers
+	public CloseableHttpResponse post(String url, String entityString, HashMap<String, String> headerMap)
+			throws ClientProtocolException, IOException {
+		CloseableHttpClient httpclient = HttpClients.createDefault(); // create http connection
+		HttpPost httpPost = new HttpPost(url); // http post request
+		httpPost.setEntity(new StringEntity(entityString)); // for payload
+
+		// Adding header to get request
+		for (Map.Entry<String, String> entry : headerMap.entrySet()) {
 			httpPost.addHeader(entry.getKey(), entry.getValue());
 		}
-			
-		CloseableHttpResponse closeableHttpResponse=httpclient.execute(httpPost); //hit the Get URL
+
+		CloseableHttpResponse closeableHttpResponse = httpclient.execute(httpPost); // hit the Get URL
 		return closeableHttpResponse;
-		}
+	}
 }
--- src/test/java/com/qa/tests/PostAPITest.java
+++ src/test/java/com/qa/tests/PostAPITest.java
@@ -18,61 +18,61 @@ import com.qa.base.TestBase;
 import com.qa.client.RestClient;
 import com.qa.data.Users;
 
-public class PostAPITest extends TestBase{
+public class PostAPITest extends TestBase {
 	TestBase testBase;
 	String serviceUrl;
 	String apiUrl;
 	String url;
 	RestClient restClient;
 	CloseableHttpResponse closeableHttpResponse;
-	
+
 	@BeforeMethod
 	public void setUp() {
 		testBase = new TestBase();
-		serviceUrl= prop.getProperty("URL");
+		serviceUrl = prop.getProperty("URL");
 		apiUrl = prop.getProperty("serviceURL");
-		url= serviceUrl+ apiUrl;
+		url = serviceUrl + apiUrl;
 	}
 
 	@Test
 	public void postAPITest() throws JsonGenerationException, JsonMappingException, IOException {
 		restClient = new RestClient();
-		HashMap<String,String> headerMap = new HashMap<String,String>();
+		HashMap<String, String> headerMap = new HashMap<String, String>();
 		headerMap.put("Content-Type", "application/json");
-		
-		//Jackson API :
+
+		// Jackson API :
 		ObjectMapper mapper = new ObjectMapper();
-		Users user = new Users("Deepak","Automation Tester");
-		
-		//Object to json file conversion
-		mapper.writeValue(new File(System.getProperty("user.dir")+"\\src\\main\\java\\com\\qa\\data\\Users.json"), user);
-		
-		//Object to json in String
-		String userJsonString=mapper.writeValueAsString(user);
+		Users user = new Users("Deepak", "Automation Tester");
+
+		// Object to json file conversion
+		mapper.writeValue(new File(System.getProperty("user.dir") + "\\src\\main\\java\\com\\qa\\data\\Users.json"),
+				user);
+
+		// Object to json in String
+		String userJsonString = mapper.writeValueAsString(user);
 		System.out.println(userJsonString);
-		
-		closeableHttpResponse= restClient.post(url, userJsonString, headerMap);
-		
-		
-		//Validate response from API
-		//StatusCode
-		int statusCode =closeableHttpResponse.getStatusLine().getStatusCode();
-		Assert.assertEquals(statusCode, TestBase.RESPONSE_STATUS_CODE_201," Status Code is not 201");
-		
-		//JSON String
-		String responseString=EntityUtils.toString(closeableHttpResponse.getEntity(), "UTF-8");
-		JSONObject responseJSON= new JSONObject(responseString);
+
+		closeableHttpResponse = restClient.post(url, userJsonString, headerMap);
+
+		// Validate response from API
+		// StatusCode
+		int statusCode = closeableHttpResponse.getStatusLine().getStatusCode();
+		Assert.assertEquals(statusCode, TestBase.RESPONSE_STATUS_CODE_201, " Status Code is not 201");
+
+		// JSON String
+		String responseString = EntityUtils.toString(closeableHttpResponse.getEntity(), "UTF-8");
+		JSONObject responseJSON = new JSONObject(responseString);
 		System.out.println("The response from API is -->" + responseJSON);
-		
-		//Json to Java Object
-		Users userResObj =mapper.readValue(responseString, Users.class);
+
+		// Json to Java Object
+		Users userResObj = mapper.readValue(responseString, Users.class);
 		System.out.println(userResObj);
-		
-		Assert.assertEquals(userResObj.getName(), user.getName(),"Name does not match");
-		
-		Assert.assertEquals(userResObj.getJob(), user.getJob(),"Name does not match");
-		
-		System.out.println("User ID-->"+userResObj.getId());
-		System.out.println("Created At-->"+userResObj.getCreatedAt());
+
+		Assert.assertEquals(userResObj.getName(), user.getName(), "Name does not match");
+
+		Assert.assertEquals(userResObj.getJob(), user.getJob(), "Job does not match");
+
+		System.out.println("User ID-->" + userResObj.getId());
+		System.out.println("Created At-->" + userResObj.getCreatedAt());
 	}
 }
